f1ec83013a23695af9eaead43ba5888b675e44bc
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-31 20:46:18 +0100

    New card style w/ BadgeIcon, new image & id

---
+++ src/assets/pokemonTypes.ts
@@ -0,0 +1,78 @@
+// COLORS: https://www.figma.com/design/uAs91a4dreOIhTrmBJVRnE/Pok%C3%A9dex-(Community
+
+import { BugBadge, GroundBadge, DarkBadge, IceBadge, DragonBadge, NormalBadge, ElectricBadge, PoisonBadge, FairyBadge, PsychicBadge, FightingBadge, RockBadge, FireBadge, SteelBadge, WaterBadge, FlyingBadge, GhostBadge, GrassBadge } from "@/assets/pokemonBadges"
+
+export const pokemonTypes = {
+  bug: {
+    badge:  BugBadge,
+    color: 'bg-[#A7B723]/50'
+  },
+  ground: {
+    badge:  GroundBadge,
+    color: 'bg-[#DEC16B]/50'
+  },
+  dark: {
+    badge:  DarkBadge,
+    color: 'bg-[#75574C]/50'
+  },
+  ice: {
+    badge:  IceBadge,
+    color: 'bg-[#9AD6DF]/50'
+  },
+  dragon: {
+    badge:  DragonBadge,
+    color: 'bg-[#7037FF]/50'
+  },
+  normal: {
+    badge:  NormalBadge,
+    color: 'bg-[#AAA67F]/50'
+  },
+  electric: {
+    badge:  ElectricBadge,
+    color: 'bg-[#F9CF30]/50'
+  },
+  poison: {
+    badge:  PoisonBadge,
+    color: 'bg-[#A43E9E]/50'
+  },
+  fairy: {
+    badge:  FairyBadge,
+    color: 'bg-[#E69EAC]/50'
+  },
+  psychic: {
+    badge:  PsychicBadge,
+    color: 'bg-[#FB5584]/50'
+  },
+  fighting: {
+    badge:  FightingBadge,
+    color: 'bg-[#C12239]/50'
+  },
+  rock: {
+    badge:  RockBadge,
+    color: 'bg-[#B69E31]/50'
+  },
+  fire: {
+    badge:  FireBadge,
+    color: 'bg-[#F57D31]/50'
+  },
+  steel: {
+    badge:  SteelBadge,
+    color: 'bg-[#B7B9D0]/50'
+  },
+  water: {
+    badge:  WaterBadge,
+    color: 'bg-[#6493EB]/50'
+  },
+  flying: {
+    badge:  FlyingBadge,
+    color: 'bg-[#A891EC]/50'
+  },
+  ghost: {
+    badge:  GhostBadge,
+    color: 'bg-[#70559B]/50'
+  },
+  grass: {
+    badge:  GrassBadge,
+    color: 'bg-[#74CB48]/50'
+  }
+}
